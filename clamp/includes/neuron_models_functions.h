2e538857e0efe2965b3fed18f301dee07a36cddc
Rodrigo Vicente Amaducci Szwarc <amaducci.rodrigo@gmail.com>
2018-07-26 13:01:03 +0200

    Wang model added (not the GUI)

--- clamp/includes/neuron_models_functions.h
+++ clamp/includes/neuron_models_functions.h
@@ -20,6 +20,7 @@ extern "C" {
 #define HR 2
 #define RLK 3
 #define GH 4
+#define WANG 5
 
 #define VAR_X 0
 #define VAR_Y 1
@@ -136,6 +137,41 @@ double ghigliazza_holmes_inf (double v, double vth, double k);
 void ghigliazza_holmes (neuron_model nm, double syn);
 
 void ghigliazza_holmes_set_pts_burst (double pts_live, neuron_model * nm);
+
+///@}
+
+/** @name Wang
+ *  Wang neuron model from (Wang, 1993). 
+ */
+///@{
+
+#define WANG_I 0
+#define WANG_DT 1
+#define WANG_G_NA 2
+#define WANG_G_NAP 3
+#define WANG_G_K 4
+#define WANG_G_KS 5
+#define WANG_G_L 6
+#define WANG_CM 7
+#define WANG_V_NA 8
+#define WANG_V_K 9
+#define WANG_V_L 10
+#define WANG_SIGMA 11
+#define WANG_PHI 12
+#define WANG_RHO 13
+#define WANG_TAUM 14
+ 
+#define WANG_H_NA 1
+#define WANG_N_K 2
+#define WANG_M_KS 3
+#define WANG_H1_KS 4
+#define WANG_H2_KS 5
+
+void wang (neuron_model nm, double syn);
+
+void wang_set_pts_burst (double pts_live, neuron_model * nm);
+
+void wang_init (neuron_model * nm, double * vars, double * params);
 
 ///@}
 
--- gui/rthybrid.cpp
+++ gui/rthybrid.cpp
@@ -179,6 +179,28 @@ void RTHybrid::on_buttonStart_clicked()
             args.params[GH_VTH_KS] = ui->doubleGhVthks->value();
 
             break;
+        case WANG:
+            args.vars = (double*) malloc (sizeof(double) * 6);
+            args.params = (double *) malloc (sizeof(double) * 15);
+
+            args.vars[VAR_X] = -66.5;
+
+            args.params[WANG_I] = 2.5;
+            args.params[WANG_G_NA] = 52.0;
+            args.params[WANG_G_NAP] = 0.1;
+            args.params[WANG_G_K] = 20.0;
+            args.params[WANG_G_KS] = 14.0;
+            args.params[WANG_G_L] = 0.1;
+            args.params[WANG_CM] = 1.0;
+            args.params[WANG_V_NA] = 55.0;
+            args.params[WANG_V_K] = -90.0;
+            args.params[WANG_V_L] = -60.0;
+            args.params[WANG_SIGMA] = 0.0;
+            args.params[WANG_PHI] = 28.57;
+            args.params[WANG_RHO] = 0.6;
+            args.params[WANG_TAUM] = 6.0;
+
+            break;
         default:
             break;
     }
